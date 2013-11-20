d25353ff0e625e33b2e9ed935400e6558d89d521
Damir R. Konkov <d4mk0f@gmail.com>
2013-11-20 23:21:50 +0400

    index.php was added with blocks

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Спорт! Спорт! Спорт!</title>
+  </head>
+  <body>
+    <div id="base_block">
+      <?php include 'blocks/header.php' ?>
+      <div id="content">
+        content
+      </div>
+      <?php include 'blocks/footer.php' ?>
+    </div>
+  </body>
+</html>
